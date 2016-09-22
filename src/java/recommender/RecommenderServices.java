87adbfa4eb80db2d2e3ad18eaf0c0a23d90513a2
Sajith <Sajith@DESKTOP-BK4FSJN>
2016-09-22 16:10:36 +0530

    Recommender services initiated

--- src/java/recommender/RecommenderServices.java
+++ src/java/recommender/RecommenderServices.java
@@ -5,10 +5,16 @@
  */
 package recommender;
 
+import java.util.List;
+
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
+import processes.RecomenderProccess;
+import objectModels.FacebookUser;
+import objectModels.Song;
+import datahandlers.FacebookDataHandler;
 /**
  *
  * @author Sajith
@@ -16,9 +22,39 @@ import javax.jws.WebParam;
 @WebService(serviceName = "RecommenderServices")
 public class RecommenderServices {
 
-    /**
-     * This is a sample web service operation
-     */
+    private boolean systemInitiated = false;
+
+    
+    @WebMethod(operationName = "getSystemStatus")
+    public boolean getSystemStatus() {
+        return RecomenderProccess.systemOnline();
+    }
+    
+    @WebMethod(operationName = "initSystem")
+    public boolean initSystem() {
+        systemInitiated = false;
+        systemInitiated = RecomenderProccess.initateApplication();
+        return systemInitiated;
+    }
+    
+    @WebMethod(operationName = "registerUser")
+    public boolean registerUser(@WebParam
+        (name = "newUser") FacebookUser newUser) {
+        return RecomenderProccess.registerUser(newUser);
+    }
+    
+    @WebMethod(operationName = "accessSystem")
+    public FacebookUser accessSystem(@WebParam(name = "uname") String uname,
+            @WebParam(name = "pword") String pword) {
+        return FacebookDataHandler.getUserObj(uname, pword);
+    }
+
+    @WebMethod(operationName = "getSongList")
+    public List<Song> getSongList(@WebParam
+        (name = "loggedUser") FacebookUser loggedUser) {
+        return RecomenderProccess.getSongList(loggedUser);
+    }
+    
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
